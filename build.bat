04c591e36d60bae7016fc66cbaa44c8b79df82e5
keen <keen@douyu.tv>
2020-08-14 19:07:55 +0800

    weather robots

---
+++ build.bat
@@ -0,0 +1,28 @@
+@echo off
+echo "made by Keen."
+
+echo "run: %0"
+
+set cur_dir=%~dp0
+set bin_dir=%cur_dir%bin
+
+go version 2>nul 1>nul
+if %errorlevel% neq 0 (
+	echo "golang not installed!"
+	goto failed
+)
+
+go mod vendor
+
+go build -mod=vendor -o %bin_dir%\WeatherNotify.exe %cur_dir%src\weather\main.go
+
+:successed
+echo ">>>>>>>>>>>>>>>> run %0 successed!"
+goto finished
+
+:failed
+echo ">>>>>>>>>>>>>>>> run %0 failed!"
+
+:finished
+
+pause
---
+++ go.mod
@@ -0,0 +1,9 @@
+module robots
+
+go 1.14
+
+require (
+	github.com/Keenjin/gomini v0.0.0-20200813065127-b9cf369f2abe
+	github.com/robfig/cron v1.2.0
+	github.com/tidwall/gjson v1.6.0
+)
